6e4f76fd04eee3c321b5dc5e092f3d528d2813dc
Zac <zacst914@gmail.com>
2013-08-04 20:05:05 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
+function file_toString (filename) {
+    var content = fs.readFileSync(filename);
+    return content.toString('utf8');
+}
+    
 app.get('/', function(request, response) {
-    response.send('Hello World 2');
+    response.send(file_toString('./index.html'));
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
